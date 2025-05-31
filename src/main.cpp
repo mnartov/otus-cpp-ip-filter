07de4fc7695d7d78e943cb70a647f36eb9aa69d1
Mikhail Nartov <102740678+mnartov@users.noreply.github.com>
2025-05-31 11:48:37 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "lib.h"
+
+int main(int, char **) 
+{
+	using namespace std;
+	
+	cout << "Version: " << version() << endl;
+	cout << "Hello, world!" << endl;
+	return 0;
+}
